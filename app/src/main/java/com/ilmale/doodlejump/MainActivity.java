b042c7196a4c2969f81293fbb07bb6fb6e16cb80
antyj1994 <antyj1994@gmail.com>
2019-01-17 16:38:42 +0100

    V0.1 runnable

--- app/src/main/java/com/ilmale/doodlejump/MainActivity.java
+++ app/src/main/java/com/ilmale/doodlejump/MainActivity.java
@@ -1,10 +1,9 @@
 package com.ilmale.doodlejump;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -53,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void launchOptionsActivity(View view) {
-        //Log.d(LOG_TAG, "Button clicked!");
+        Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, OptionsActivity.class);
+        startActivity(intent);
     }
 }
