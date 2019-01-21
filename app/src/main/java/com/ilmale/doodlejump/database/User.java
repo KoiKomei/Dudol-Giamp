f2cd760fc835b7185eb0459093032012d267d3c4
KoiKomei <alex.parisella@gmail.com>
2019-01-21 17:46:40 +0100

    BASE DEL DATABASE

---
+++ app/src/main/java/com/ilmale/doodlejump/database/User.java
@@ -0,0 +1,53 @@
+package com.ilmale.doodlejump.database;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "User")
+public class User {
+
+    @PrimaryKey
+    private int id;
+
+    @ColumnInfo(name="Nome_Utente")
+    private String username;
+
+    @ColumnInfo(name="Password")
+    private String password;
+
+    @ColumnInfo(name="Email")
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
