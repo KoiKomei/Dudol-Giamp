4435ca1727bcb36e91ac8f88fb999d93b690f336
KoiKomei <alex.parisella@gmail.com>
2019-01-21 19:20:22 +0100

    Deep inside of me

--- app/src/main/java/com/ilmale/doodlejump/AddUserFragment.java
+++ app/src/main/java/com/ilmale/doodlejump/AddUserFragment.java
@@ -8,6 +8,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.ilmale.doodlejump.database.User;
 
 
 /**
@@ -26,7 +29,41 @@ public class AddUserFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_user, container, false);
+        View view= inflater.inflate(R.layout.fragment_add_user, container, false);
+        userId=view.findViewById(R.id.txt_user_id);
+        userEmail=view.findViewById(R.id.txt_user_email);
+        userName=view.findViewById(R.id.txt_user_name);
+        userPassword=view.findViewById(R.id.txt_user_password);
+        bnRegister=view.findViewById(R.id.bn_register);
+
+        bnRegister.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                int UserId=Integer.parseInt(userId.getText().toString());
+                String UserName=userName.getText().toString();
+                String UserEmail=userEmail.getText().toString();
+                String UserPassword=userPassword.getText().toString();
+
+                User user=new User();
+                user.setEmail(UserEmail);
+                user.setId(UserId);
+                user.setPassword(UserPassword);
+                user.setUsername(UserName);
+
+                RegisterActivity.db.ourDao().setUser(user);
+                Toast.makeText(getActivity(), "user added successfully", Toast.LENGTH_SHORT).show();
+
+                userId.setText("");
+                userEmail.setText("");
+                userName.setText("");
+                userPassword.setText("");
+                
+
+            }
+
+        });
+        return view;
     }
 
 }
--- app/src/main/java/com/ilmale/doodlejump/RegisterActivity.java
+++ app/src/main/java/com/ilmale/doodlejump/RegisterActivity.java
@@ -1,19 +1,24 @@
 package com.ilmale.doodlejump;
 
+import android.arch.persistence.room.Room;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ilmale.doodlejump.database.OurDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
 
     public static FragmentManager fm;
+    public static OurDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+        String name="userdb";
         fm=getSupportFragmentManager();
+        db= Room.databaseBuilder(getApplicationContext(), OurDatabase.class, name).allowMainThreadQueries().build();
 
         if(findViewById(R.id.fragment_container)!=null){
 
