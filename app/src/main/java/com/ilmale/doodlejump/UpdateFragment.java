38235a0ca3c7caf9d469577a8609a696ea589e98
KoiKomei <alex.parisella@gmail.com>
2019-01-24 20:21:46 +0100

    What you do to me

---
+++ app/src/main/java/com/ilmale/doodlejump/UpdateFragment.java
@@ -0,0 +1,62 @@
+package com.ilmale.doodlejump;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.ilmale.doodlejump.database.User;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UpdateFragment extends Fragment {
+
+    private EditText userEmail, userPassword, newPass;
+    private Button bnUpdate;
+
+    public UpdateFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view= inflater.inflate(R.layout.fragment_update, container, false);
+
+        userEmail=view.findViewById(R.id.txt_email_update);
+        userPassword=view.findViewById(R.id.txt_old_password);
+        newPass=view.findViewById(R.id.txt_new_password);
+        bnUpdate=view.findViewById(R.id.bn_update);
+
+        bnUpdate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String email=userEmail.getText().toString();
+                String oldp=userPassword.getText().toString();
+                String newp=newPass.getText().toString();
+                RegisterActivity.db.ourDao().updatePass(email, newp, oldp);
+                userEmail.setText("");
+                userPassword.setText("");
+                newPass.setText("");
+
+            }
+
+
+
+        });
+
+        return view;
+
+    }
+
+
+
+}
--- app/src/main/java/com/ilmale/doodlejump/database/OurDao.java
+++ app/src/main/java/com/ilmale/doodlejump/database/OurDao.java
@@ -2,11 +2,30 @@ package com.ilmale.doodlejump.database;
 
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
 
 @Dao
 public interface OurDao {
 
     @Insert
     void setUser(User user);
+
+    @Query("select * from User")
+    public List<User> getUsers();
+
+    @Delete
+    public void deleteUser(User user);
+
+    @Update
+    public void updateUser(User user);
+
+    @Query("UPDATE User SET Password=:newPass WHERE Email=:email AND Password=:oldPass")
+    public void updatePass(String email, String newPass, String oldPass);
+
+
 }
