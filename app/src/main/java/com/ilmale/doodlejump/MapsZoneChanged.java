06f3b32ddd35ce6e9b4b9641e9bdcde03049012b
luca2808 <32547791+luca2808@users.noreply.github.com>
2019-02-02 09:09:21 +0200

    notifiche

---
+++ app/src/main/java/com/ilmale/doodlejump/MapsZoneChanged.java
@@ -0,0 +1,97 @@
+package com.ilmale.doodlejump;
+
+import android.annotation.TargetApi;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+
+
+public class MapsZoneChanged {
+
+    private static final String NOTIFICATION_TAG = "MapsZoneChanged";
+
+    public static void notify(final Context context, final int number) {
+        final Resources res = context.getResources();
+
+        final Bitmap picture = BitmapFactory.decodeResource(res, R.drawable.example_picture);
+
+        final String title = res.getString(R.string.maps_zone_changed_notification_title);
+        final String text = res.getString(R.string.maps_zone_changed_notification_text);
+
+        final NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+
+                // Set appropriate defaults for the notification light, sound, and vibration.
+                .setDefaults(Notification.DEFAULT_ALL)
+
+                // Set required fields, including the small icon, the notification title, and text.
+                .setSmallIcon(R.drawable.ic_stat_maps_zone_changed)
+                .setContentTitle(title)
+                .setContentText(text)
+
+                // Set the pending intent to be initiated when the user touches
+                // the notification.
+                .setContentIntent(PendingIntent.getActivity(context,0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT))
+
+                // Show expanded text content on devices running Android 4.1 or
+                // later.
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText(text)
+                        .setBigContentTitle(title)
+                        .setSummaryText("Dummy summary text"))
+
+                // Example additional actions for this notification. These will
+                // only show on devices running Android 4.1 or later, so you
+                // should ensure that the activity in this notification's
+                // content intent provides access to the same actions in
+                // another way.
+                .addAction(
+                        R.drawable.ic_action_stat_share,
+                        res.getString(R.string.action_share),
+                        PendingIntent.getActivity(
+                                context,
+                                0,
+                                Intent.createChooser(new Intent(Intent.ACTION_SEND)
+                                        .setType("text/plain")
+                                        .putExtra(Intent.EXTRA_TEXT, "Dummy text"), "Dummy title"),
+                                PendingIntent.FLAG_UPDATE_CURRENT))
+                .addAction(
+                        R.drawable.ic_action_stat_reply,
+                        res.getString(R.string.action_reply),
+                        null)
+
+                // Automatically dismiss the notification when it is touched.
+                .setAutoCancel(true);
+
+        notify(context, builder.build());
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    private static void notify(final Context context, final Notification notification) {
+        final NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.notify(NOTIFICATION_TAG, 0, notification);
+        } else {
+            nm.notify(NOTIFICATION_TAG.hashCode(), notification);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.ECLAIR)
+    public static void cancel(final Context context) {
+        final NotificationManager nm = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
+            nm.cancel(NOTIFICATION_TAG, 0);
+        } else {
+            nm.cancel(NOTIFICATION_TAG.hashCode());
+        }
+    }
+}
